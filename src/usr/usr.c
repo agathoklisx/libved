c1b95d686ef20dedf0b67cf1da76830c3983da69
αγαθοκλής <aga.chatzimanikas@gmail.com>
2019-11-13 19:58:50 +0200

    Implement a `stat command in the user namespace. With regards to output, this is an almost (like) stat command. The difference is that in the case of links, this command shows also and information about the link itself. Note that the ouput is redirected to the scratch buffer.

--- src/usr/usr.c
+++ src/usr/usr.c
@@ -9,6 +9,9 @@
 #endif /* SYS_NAME */
 
 #include <sys/stat.h> /* for mkdir() */
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
 #include "../lib/utf8/is_utf8.c"
 
@@ -430,12 +433,92 @@ private int sys_mkdir (char *dir, mode_t mode, int verbose) {
   return NOTOK;
 }
 
+private string_t *__readlink__ (char *obj) {
+  string_t *link = String.new (PATH_MAX);
+
+  int loops = 0;
+readthelink:
+  link->num_bytes = readlink (obj, link->bytes, link->mem_size);
+  if (NOTOK is (ssize_t) link->num_bytes)
+    return link;
+
+  if (link->num_bytes is link->mem_size and loops++ is 0) {
+    String.reallocate (link, (link->mem_size / 2));
+    goto readthelink;
+  }
+
+  link->bytes[link->num_bytes] = '\0'; // readlink() does not append the nullbyte
+  return link;
+}
+
+private int sys_stat (buf_t **thisp, char *obj) {
+  struct stat st;
+  if (NOTOK is lstat (obj, &st)) {
+    Msg.error ($myed, "failed to lstat() file, %s", Error.string ($myed, errno));
+    return NOTOK;
+  }
+
+  Ed.append.toscratch ($myed, CLEAR, "==- stat output -==");
+
+  int islink = S_ISLNK (st.st_mode);
+  string_t *link = NULL;
+
+  if (islink) {
+    link = __readlink__ (obj);
+    if (NOTOK is (ssize_t) link->num_bytes) {
+      Msg.error ($myed, "readlink(): %s", Error.string ($myed, errno));
+      Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "  File: %s", obj);
+    } else
+      Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "  File: %s -> %s", obj, link->bytes);
+  } else
+    Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "  File: %s", obj);
+
+theoutput:
+  Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "  Size: %ld,  Blocks: %ld,  I/O Block: %ld",
+      st.st_size, st.st_blocks, st.st_blksize);
+  Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "Device: %ld,  Inode: %ld,  Links: %d",
+      st.st_dev, st.st_ino, st.st_nlink);
+
+  char mode_string[16];
+  Vsys.stat.mode_to_string (mode_string, st.st_mode);
+  char mode_oct[8]; snprintf (mode_oct, 8, "%o", st.st_mode);
+  struct passwd *pswd = getpwuid (st.st_uid);
+  struct group *grp = getgrgid (st.st_gid);
+  Ed.append.toscratch_fmt ($myed, DONOT_CLEAR,
+  	"Access: (%s/%s), Uid: (%ld / %s), Gid: (%d / %s)\n",
+     mode_oct+2, mode_string, st.st_uid,
+    (NULL is pswd ? "NONE" : pswd->pw_name), st.st_gid,
+    (NULL is grp  ? "NONE" : grp->gr_name));
+  time_t atm = (long int) st.st_atime;
+  Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "       Last Access: %s", Cstring.trim.end (ctime (&atm), '\n'));
+  time_t mtm = (long int) st.st_mtime;
+  Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, " Last Modification: %s", Cstring.trim.end (ctime (&mtm), '\n'));
+  time_t ctm = (long int) st.st_ctime;
+  Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "Last Status Change: %s\n", Cstring.trim.end (ctime (&ctm), '\n'));
+
+  if (islink and NULL isnot link) {
+    islink = 0;
+    ifnot (Path.is_absolute (link->bytes)) {
+      char *dname = Path.dirname (obj);
+      String.prepend_fmt (link, "%s/", dname);
+      free (dname);
+    }
+    obj = link->bytes;
+    Ed.append.toscratch ($myed, DONOT_CLEAR, "==- Link info -==");
+    Ed.append.toscratch_fmt ($myed, DONOT_CLEAR, "  File: %s", obj);
+    if (OK is stat (link->bytes, &st)) goto theoutput;
+  }
+  String.free (link);
+  Ed.scratch ($myed, thisp, NOT_AT_EOF);
+  return OK;
+}
+
 private void __u_add_rline_sys_commands__ (ed_t *this) {
  /* sys defined commands can begin with '`': associated with shell syntax */
-  int num_commands = 2;
-  char *commands[] = {"`mkdir", "`man", NULL};
-  int num_args[] = {3, 0, 0};
-  int flags[] = {RL_ARG_FILENAME|RL_ARG_VERBOSE, 0, 0};
+  int num_commands = 3;
+  char *commands[] = {"`mkdir", "`man", "`stat", NULL};
+  int num_args[] = {3, 0, 1, 0};
+  int flags[] = {RL_ARG_FILENAME|RL_ARG_VERBOSE, 0, RL_ARG_FILENAME, 0};
   Ed.append.rline_commands (this, commands, num_commands, num_args, flags);
   Ed.append.command_arg (this, "`man", "--section=", 10);
   Ed.append.command_arg (this, "`mkdir", "--mode=", 7);
@@ -474,6 +557,12 @@ private int __u_rline_cb__ (buf_t **thisp, rline_t *rl, utf8 c) {
 
     retval = sys_man (thisp, names->head->data->bytes, sect_id);
     Vstring.free (names);
+
+  } else if (Cstring.eq (com->bytes, "`stat")) {
+    vstr_t *fnames = Rline.get.arg_fnames (rl, 1);
+    if (NULL is fnames) goto theend;
+    retval = sys_stat (thisp, fnames->head->data->bytes);
+    Vstring.free (fnames);
 
   } else if (Cstring.eq (com->bytes, "@validate_utf8")) {
     vstr_t *words = Rline.get.arg_fnames (rl, 1);
